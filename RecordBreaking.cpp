e435798f52f0ca545acd8e54547127db73792017
ravikant0055 <kumarra927@gmail.com>
2021-05-18 01:41:44 +0530

    update

--- RecordBreaking.cpp
+++ RecordBreaking.cpp
@@ -6,10 +6,34 @@ int main()
     cout<<"Enter the num of element in array : ";
     cin>>n;
     
-    int arr[n];
+    int arr[n+1];
+    arr[n] = -1;
+   
     cout<<"Enter the value of array"<<endl;
     for(int i=0; i<n; i++)
     {
         cin>>arr[i];
     }
+
+    if(n == 1)
+    {
+        cout<<"1"<<endl;
+        return 0;
+    }
+
+    int ans = 0;
+    int maxnum = -1;
+
+    for(int i=0 ; i<n; i++)
+    {
+        if(arr[i]>maxnum && arr[i]>arr[i+1])
+        {
+            ans++;
+        }
+        maxnum = max(maxnum, arr[i]); 
+    }
+
+    cout<<"Record breaking days : "<<ans<<endl;
+
+    return 0;
 }
