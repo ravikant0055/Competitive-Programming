1d130b3a5eba66df9e424348b4a1bc177e0eb49b
ravikant0055 <kumarra927@gmail.com>
2021-05-17 21:27:24 +0530

    commit

--- RecordBreaking.cpp
+++ RecordBreaking.cpp
@@ -1,2 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the num of element in array : ";
+    cin>>n;
+    
+    int arr[n];
+    cout<<"Enter the value of array"<<endl;
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+}
