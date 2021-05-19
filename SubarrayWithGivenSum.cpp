830fa73dcc11796d78895415c678e685b03933a7
ravikant0055 <kumarra927@gmail.com>
2021-05-19 21:59:59 +0530

    commit

---
+++ SubarrayWithGivenSum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the num of element in array : "<<endl;
+    cin>>n;
+    
+
+    int arr[n];
+
+    cout<<"Enter the value of array "<<endl;
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+
+
+   
+   return 0;
+
+}
