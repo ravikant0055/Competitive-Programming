3f9eca49f4bf205db215f5c53c2cf2f33253407c
ravikant0055 <kumarra927@gmail.com>
2021-05-20 22:13:21 +0530

    update

--- SubarrayWithGivenSum.cpp
+++ SubarrayWithGivenSum.cpp
@@ -2,21 +2,56 @@
 using namespace std;
 int main()
 {
-    int n;
-    cout<<"Enter the num of element in array : "<<endl;
+    int n,s;
+    cout<<"Enter the num of element in array : ";
     cin>>n;
-    
+
+    cout<<"Give a sum for comparison : ";
+    cin>>s;
 
     int arr[n];
-
     cout<<"Enter the value of array "<<endl;
     for(int i=0; i<n; i++)
     {
         cin>>arr[i];
     }
+    
+    int i=0, j=0, st=-1, ed=-1, sum=0;
 
+    while(j<n && sum+arr[j]<=s)
+    {
+        sum+=arr[j];
+        j++;
+    } 
+    
+    if(sum == s)
+    {
+        cout<<"Matched Subarray is :"<<i+1<<" "<<j<<endl;
+        return 0;
+    }
 
-   
+    while(j<n)
+    {
+        sum+=arr[j];
+        
+        while(sum > s)
+        {
+            sum-=arr[i];
+            i++;
+        }
+
+        if( sum == s )
+        {
+           st = i+1;
+           ed = j+1;
+           break;
+        }
+        
+        j++;
+    }
+
+   cout<<"Matched Subarray is :"<<st<<" "<<ed<<endl;
+
    return 0;
 
 }
