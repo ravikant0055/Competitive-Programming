2e2f09250176dbbe6f397a8b8d98db7d2a9eb880
ravikant0055 <kumarra927@gmail.com>
2021-05-21 21:33:38 +0530

    commit

---
+++ SmallestPositiveMissingNum.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the num of element in array : ";
+    cin>>n;
+    
+    int arr[n];
+    cout<<"Enter the value of array"<<endl;
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    
+}
